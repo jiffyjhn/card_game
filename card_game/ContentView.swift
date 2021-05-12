62abd20b7a1911852a06fc216593234eca9630a5
Hao <hsu2@apple.com>
2021-05-12 10:28:13 -0700

    Initial Commit

---
+++ card_game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  card_game
+//
+//  Created by Hao Su on 5/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ card_game/card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_gameApp.swift
+//  card_game
+//
+//  Created by Hao Su on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
